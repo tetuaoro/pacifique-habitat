d44a4e1cb7dd983a1c8701b1d40b3a834069a194
tetuaoro <tetuaoropro@gmail.com>
2022-06-04 15:27:42 -1000

    feat: add sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://pacifique-habitat.vercel.app",
+  generateRobotsTxt: true,
+  changefreq: "monthly",
+}
+
+module.exports = config
